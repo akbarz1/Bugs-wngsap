c5f7570fcf890f35fb09e1ba700181e8d4d61e9f
akbarz1 <sitiimut344@gmail.com>
2025-11-28 08:00:37 +0700

    Create config.js

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8219628196:AAEj65b9FgT0l8zUbMAUeyBCrRSVC80XeIc",  // Masukin Bot token kamu
+  owners: "8385539137", // Masukin ID Telegram kamu
+  port: "2772", // Masukin Port panel kamu 
+  ipvps: "Http://tokisaki.panel.husni.izumivera.my.id" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1710-1711 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
